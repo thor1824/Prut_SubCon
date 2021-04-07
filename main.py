bd5d3ad499c005097e44379ad1b92e559639e5a2
Frederik Jensen <frederikhj0@gmail.com>
2021-04-07 11:25:38 +0200

    Can now get from api

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from packages.Implementation.synonymizer import synonymizer
+from packages.Implementation.data_source import DataSource
 
 
 def print_hi(name):
--- packages/Implementation/data_source.py
+++ packages/Implementation/data_source.py
@@ -1,7 +1,13 @@
 from ..Entities.synonym import Synonym
 from ..Interfaces.synonym_data_source_interface import ISynonymDataSource
+import requests
+import json
 
 class DataSource(ISynonymDataSource):
     def get_synonyms(self, searchTerm: str) -> list[Synonym]:
-        """TODO"""
+        response = requests.get("https://api.datamuse.com/words?rel_syn=" + searchTerm)
+        if (response.ok):
+            return response.json()
+        else:
+            return []
         pass
