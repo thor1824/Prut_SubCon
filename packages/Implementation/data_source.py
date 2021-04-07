0c08ac3698627a252a28d99bf2277bff3d82abf6
Thorbjørn S. Damkjær <thor1824@gmail.com>
2021-04-07 12:07:47 +0200

    partial get_synonym

--- packages/Implementation/data_source.py
+++ packages/Implementation/data_source.py
@@ -3,9 +3,10 @@ from ..Interfaces.synonym_data_source_interface import ISynonymDataSource
 import requests
 import json
 
+
 class DataSource(ISynonymDataSource):
     def get_synonyms(self, searchTerm: str) -> list[Synonym]:
-        response = requests.get("https://api.datamuse.com/words?rel_syn=" + searchTerm)
+        """response = requests.get("https://api.datamuse.com/words?rel_syn=" + searchTerm)
         if (response.ok):
             synonyms = []
             data = response.json()
@@ -17,4 +18,5 @@ class DataSource(ISynonymDataSource):
             return synonyms
         else:
             return []
-        pass
+        pass"""
+        return [Synonym('wooden', 1), Synonym('woods', 1), Synonym('woodwind', 1)]
--- packages/Implementation/synonymizer.py
+++ packages/Implementation/synonymizer.py
@@ -1,12 +1,55 @@
+from ..Entities.synonym import Synonym
 from ..Interfaces.synonymizer_interface import ISynonymizer
 from ..Interfaces.synonym_data_source_interface import ISynonymDataSource
 
+
+def __levenshtein_distance__(s: str, t: str) -> int:
+    # Initialize matrix of zeros
+    rows = len(s) + 1
+    cols = len(t) + 1
+    distance = [[0 for i in range(cols)] for j in range(rows)]
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            distance[i][0] = i
+            distance[0][j] = j
+
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row - 1] == t[col - 1]:
+                cost = 0
+            else:
+                cost = 1
+
+            distance[row][col] = min(distance[row - 1][col] + 1,  # Cost of deletions
+                                     distance[row][col - 1] + 1,  # Cost of insertions
+                                     distance[row - 1][col - 1] + cost)  # Cost of substitutions
+    return distance[row][col]
+
+
 class synonymizer(ISynonymizer):
+    __data_source: ISynonymDataSource = None
+    """request_dict = dict()"""
+    __request_dict = {'Wood': [Synonym('wooden', 1), Synonym('woods', 1), Synonym('woodwind', 1)]}
+
     def set_synonym_data_source(self, data: ISynonymDataSource) -> None:
         """Dependency injection of the data source"""
-        pass
+        self.__data_source = data
 
-    def get_synonyms(self, searchTerm: str) -> list[str]:
+    def get_synonyms(self, search_term: str) -> list[str]:
+        try:
+            result = self.__request_dict[search_term]
+            return result
+        except:
+            if self.__data_source is None:
+                raise Exception('Not yet initialized properly')
+
+            result2 = self.__data_source.get_synonyms(search_term)
+        result2.sort(key=lambda x: __levenshtein_distance__(search_term, x.word))
+        if len(result2) == 3:
+            return result2
+
+        
         """Get the synonyms from the data source. Calculate levenshtein distance and return top 3 results"""
         """Must be sorted on levenshtein distance (lowest first)"""
         """The result must be locally cached in memory for future requests"""
@@ -20,25 +63,5 @@ class synonymizer(ISynonymizer):
         """Returns the most searched term. Must be O(1)"""
         pass
 
-    def __levenshtein_distance__(self, s, t):
-        # Initialize matrix of zeros
-        rows = len(s) + 1
-        cols = len(t) + 1
-        distance = [[0 for i in range(cols)] for j in range(rows)]
-
-        for i in range(1, rows):
-            for j in range(1, cols):
-                distance[i][0] = i
-                distance[0][j] = j
-
-        for col in range(1, cols):
-            for row in range(1, rows):
-                if s[row - 1] == t[col - 1]:
-                    cost = 0
-                else:
-                    cost = 1
-
-                distance[row][col] = min(distance[row - 1][col] + 1,  # Cost of deletions
-                                         distance[row][col - 1] + 1,  # Cost of insertions
-                                         distance[row - 1][col - 1] + cost)  # Cost of substitutions
-        return distance[row][col]
+    def __cache_result(self, search_key: str, result: list[Synonym]):
+        self.__request_dict[search_key] = result
